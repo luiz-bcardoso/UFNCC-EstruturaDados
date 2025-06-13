58b4328a430722f6269bef02c03fab04da4ee292
Luiz Batista Cardoso <luizbatistacardoso@gmail.com>
2025-06-13 10:48:42 -0300

    Update Arvore.h

--- trabalho_estrutura_dados_nene/Arvore.h
+++ trabalho_estrutura_dados_nene/Arvore.h
@@ -1,114 +1,177 @@
-#include <cstddef>
-#include <corecrt_malloc.h>
-#include <iostream>
-
 typedef struct no {
-	int dado;
-	struct no* esq;
-	struct no* dir;
+    int dado;
+    struct no *esq;
+    struct no *dir;
 } Arvore;
-
-Arvore* inserir(int valor, Arvore* raiz) {
-	if (raiz) {
-		if (valor < raiz->dado) {
-			// Ir para esquerda
-			raiz->esq = inserir(valor, raiz->esq);
-		}
-		else {
-			// Ir para direita
-			raiz->dir = inserir(valor, raiz->dir);
-		}
-		return raiz;
-	}
-	else {
-		Arvore* novo = (Arvore*)malloc(sizeof(Arvore));
-		novo->dado = valor;
-		novo->esq = NULL;
-		novo->dir = NULL;
-		return novo;
-	}
+ 
+ 
+Arvore *inserir(int dado, Arvore *raiz) {
+    if (raiz) {
+        if (dado < raiz->dado) { //ir para esquerda
+            raiz->esq = inserir(dado, raiz->esq);
+        } else { //ir para direita
+            raiz->dir = inserir(dado, raiz->dir);
+        }
+        return raiz;
+    }
+    //processo chegou no vazio da árvore e um novo nodo sera adicionado
+    Arvore *novo = (Arvore*)malloc(sizeof(Arvore));
+    novo->dado = dado;
+    novo->esq = NULL;
+    novo->dir = NULL;
+    return novo;
 }
-
-void exibirOrdenado(Arvore* raiz) {
-	if (raiz) { // percurso in-fixado ou ERD
-		exibirOrdenado(raiz->esq);
-		std::cout << raiz->dado << "\t";
-		exibirOrdenado(raiz->dir);
-	}
+ 
+void exibirOrdenado(Arvore *raiz) {
+    if (raiz) { //caminhamento infixado = ERD
+        exibirOrdenado(raiz->esq);
+        cout << raiz->dado << "\t";
+        exibirOrdenado(raiz->dir);
+    }
 }
-
-int contar(Arvore* raiz) {
-	if (raiz) {
-		return contar(raiz->esq) + contar(raiz->dir);
-	}
-	else {
-		return 0;
-	}
+ 
+void exibirArvore(int nivel, Arvore *raiz) {
+    if (raiz) { //percurso DRE
+        //vai para Direita
+        exibirArvore(nivel + 1, raiz->dir);
+ 
+        //visita rairz
+        for (int i = 0; i < nivel; i++) {
+            cout << "  ";
+        }
+        cout << "(" << nivel << ")" <<  raiz->dado << endl;
+ 
+        //vai para Esquerda
+        exibirArvore(nivel + 1, raiz->esq);
+    }
 }
-
-int somar(Arvore* raiz) {
-	if (raiz) {
-		return raiz->dado + somar(raiz->esq) + somar(raiz->dir);
-	}
-	else{
-		return 0;
-	}
+ 
+int contarNodos(Arvore *raiz) {
+    if (raiz) {
+        return 1 + contarNodos(raiz->esq) + contarNodos(raiz->dir);
+    }
+    return 0;
 }
-
-int contarPares(Arvore* raiz) {
-	if (raiz) {
-		int cont = 0;
-		if (raiz->dado % 2 == 0) {
-			cont = 1;
-		}
-		return cont + contarPares(raiz->esq) + contarPares(raiz->dir);
-	}
-	else {
-		return 0;
-	}
+ 
+bool localizar(int dado, Arvore *raiz) {
+    if (raiz) {
+        if (dado == raiz->dado) {
+            return true;
+        }
+        if (dado < raiz->dado) { //ir para a esquerda
+            return localizar(dado, raiz->esq);
+        } //ir para direita
+        return localizar(dado, raiz->dir);
+    }
+    return false;
 }
-
-void exibirFolhas(Arvore* raiz) {
-	if (raiz) {
-		if (!raiz->esq && !raiz->dir) {
-			std::cout << raiz->dado << "\t"; // É uma folha
-		}
-		else {
-			exibirFolhas(raiz->esq);
-			exibirFolhas(raiz->dir);
-		}
-	}
-	else {
-		std::cout << "Árvore vazia." << std::endl;
-	}
+ 
+int localizarNivel(int dado, Arvore *raiz) {
+    if (raiz) {
+        if (dado == raiz->dado){
+            return 0;
+        }
+        if (dado < raiz->dado) { //ir para esquerda
+            int respostaDoEmpilhamento = localizarNivel(dado, raiz->esq);
+            if (respostaDoEmpilhamento == -1) {
+                return -1;
+            }
+            return 1 + respostaDoEmpilhamento;
+ 
+        } else { //ir para direita
+            int respostaDoEmpilhamento = localizarNivel(dado, raiz->dir);
+            if (respostaDoEmpilhamento == -1) {
+                return -1;
+            }
+            return 1 + respostaDoEmpilhamento;
+        }
+    }
+    return -1; //cheguei no vazio e nao encontrei o valor
 }
-
-int contarFolhas(Arvore* raiz) {
-	if (raiz) {
-		if (!raiz->esq && !raiz->dir) {
-			return 1; // É uma folha
-		}
-		else {
-			return contarFolhas(raiz->esq) + contarFolhas(raiz->dir);
-		}
-	}
-	else {
-		return 0; // Árvore vazia
-	}
+ 
+void exibirFolhas(Arvore *raiz) {
+    if (raiz) {
+        if (!raiz->esq && !raiz->dir) {
+            cout << raiz->dado << "\t";
+        } else {
+            exibirFolhas(raiz->esq);
+            exibirFolhas(raiz->dir);
+        }
+    }
 }
-
-bool localizar(int valor, Arvore* raiz) {
-	if (raiz) {
-		if (raiz->dado == valor) {
-			return true;
-		}
-		else if (valor < raiz->dado) {
-			return localizar(valor, raiz->esq);
-		}
-		else {
-			return localizar(valor, raiz->dir);
-		}
-	}
-	else {
-		return false;
-	}
+ 
+int contarFolhas(Arvore *raiz) {
+    if (raiz) {
+        if (!raiz->esq && !raiz->dir) {
+            return 1;
+        }
+        return 0 + contarFolhas(raiz->esq) + contarFolhas(raiz->dir);
+    }
+    return 0;
+}
+ 
+Arvore *podar(Arvore *raiz) {
+    if (raiz) {
+        if (!raiz->esq && !raiz->dir) {
+            //apagar o no folha
+            free(raiz);
+            return NULL;
+        }
+        raiz->esq = podar(raiz->esq);
+        raiz->dir = podar(raiz->dir);
+        return raiz;
+    }
+    return NULL;
+}
+ 
+Arvore *limpar(Arvore *raiz) {
+    if (raiz) {
+        raiz->esq = limpar(raiz->esq);
+        raiz->dir = limpar(raiz->dir);
+        free(raiz);
+    }
+    return NULL;
+}
+ 
+Arvore *remover(int dado, Arvore *raiz) {
+    if (raiz) {
+        if (dado == raiz->dado) {//localizei o dado e remover
+            //verificar se eh folha
+            if (!raiz->esq && !raiz->dir) {
+                free(raiz);
+                return NULL;
+            }
+            //verificar se ha um filho somente
+            if ((raiz->esq && !raiz->dir) || (raiz->dir && !raiz->esq)) {
+                Arvore *tmp;
+                if (raiz->esq) {
+                    tmp = raiz->esq;    
+                }
+                if (raiz->dir) {
+                    tmp = raiz->dir;    
+                }
+                free(raiz);
+                return tmp;
+            }
+            //se estou neste ponto eh porque o nodo tem dois filhos e o filho da esquerda serah o novo pai
+            Arvore *novoPai = raiz->esq;
+            Arvore *tmp = novoPai->dir;
+            novoPai->dir = raiz->dir;
+            free(raiz);
+ 
+            Arvore *p;
+            for (p = novoPai->dir; p->esq; p = p->esq);
+           
+            p->esq = tmp;
+            return novoPai;
+        }
+        if (dado < raiz->dado) {
+            raiz->esq = remover(dado, raiz->esq);
+        } else {
+            raiz->dir = remover(dado, raiz->dir);
+        }
+        return raiz;
+    }
+    return NULL;
+}
+ 
